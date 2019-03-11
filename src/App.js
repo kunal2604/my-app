c9cc66e948e2773b21ef569128e38ccf493371d5
Kunal Kumar <kmrknl2604@gmail.com>
2019-03-11 14:17:52 +0530

    Modified initial files

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Ho Ho Ho! Edit <code>src/App.js</code> and you know what!! Save it to reload.
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hey bro, don't ever Learn React :-D
           </a>
         </header>
       </div>
